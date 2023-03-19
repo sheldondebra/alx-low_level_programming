f7371d4ccfe38fa19e19119cc02d98f9af11c1c9
Sheldondebra <kofisheldon@gmail.com>
2023-03-18 23:03:13 -0700

    alx-low_level_programming

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * Main - print if the number is positive, zero , or negative
+ *
+ * Description: This program will use the main function to print if
+ * a number is positive, negative or zero
+ *
+ * Return : Returns 0
+ *
+ */
+
+int main(void)
+{
+	int n;
+	int l;
+
+	srand(time(0));
+	n = road() - RAND_MAX / 2;
+	l = n % 10;
+
+	if (1 > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	}
+	
+	else if (l == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, l);
+	}		
+	else
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	return (0);
+	}
