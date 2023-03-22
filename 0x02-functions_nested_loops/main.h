747ba2f7c0b0e370e62dd58328a44bcdb6fe2f53
sheldondebra <54963942+sheldondebra@users.noreply.github.com>
2023-03-22 05:36:45 +0000

    main h updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,18 @@
-int_putchar(char);
-void print_alphapbet(void);
-void print_alphabet_x10(void);
-int_islower(int c);
-int_is alpha(int c);
-int print_sign(int n);
-int_abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void num(int r, int n);
-void print_times_table(int n);
-void print to 98(int n);
+#ifndef HOLBERTON H INCLUDED
+#define HOLBERTON H INCLUDED
 
+int _putchar(char);
+void print_alphabet (void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c); 
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer (void); 
+void times_table(void);
+int add(int a, int b); 
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
 #endif
