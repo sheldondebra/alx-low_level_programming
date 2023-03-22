c41ad89c748210730b0af48d148f26c5519f9a77
Sheldondebra <kofisheldon@gmail.com>
2023-03-21 22:27:49 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+int_putchar(char);
+void print_alphapbet(void);
+void print_alphabet_x10(void);
+int_islower(int c);
+int_is alpha(int c);
+int print_sign(int n);
+int_abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void num(int r, int n);
+void print_times_table(int n);
+void print to 98(int n);
+
+#endif
