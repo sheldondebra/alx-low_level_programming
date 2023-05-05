23221b33c360c34b51ce2aa536202600ef9be427
sheldondebra <kofisheldon@gmail.com>
2023-05-05 05:28:14 +0000

    updated Bit

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,15 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * get_endianness - checks endianess.
-  * Return: 0 if big endian, 1 if little endian.
-  */
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
 int get_endianness(void)
 {
-	int i = 1;
-	char *ch = (char *)&i;
+	int x;
+	char *y;
 
-	if (*ch)
-		return (1);
-	return (0);
+	x = 1;
+	y = (char *)&x;
+	return (*y);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,16 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * set_bit - sets the value of a bit to 1 at a given index.
+ * @n: number to set
+ * @index: index at which to set bit
  *
- * @n: address of n
- * @index: position
- * Return: 1 if it worked, or -1 if an error occurred.
+ * Return: 1 if it worked, or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 64)
+	unsigned long int set;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	*n ^= (1 << index);
+	set = 1 << index;
+	*n = *n | set;
 	return (1);
 }
